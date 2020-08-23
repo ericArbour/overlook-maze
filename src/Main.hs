76689aba023841696a6ea5324d11bcefd605ed06
Eric Arbour <arboureric@gmail.com>
2020-08-23 16:41:59 -0400

    Add placeholder app state.

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,24 @@
 
 module Main where
 
+import Data.Text (pack, Text)
 
 import           Shpadoinkle
 import           Shpadoinkle.Backend.ParDiff
-import           Shpadoinkle.Html
+import           Shpadoinkle.Html (getBody, div_, h1_)
 
+data AppState = AppState { greeting :: String } deriving (Eq, Show)
 
-view :: () -> Html ()
-view _ = "hello world"
+initialAppState :: AppState
+initialAppState = AppState { greeting = "Hello, Newman" }
+
+view :: AppState -> Html AppState
+view appState = div_
+  [ h1_ [ "The Overlook Maze" ]
+  , div_ [ text . pack $ greeting appState ]
+  ]
 
 
 main :: IO ()
 main = runJSorWarp 8080 $
-  simple runParDiff () (constly' . view) getBody
+  simple runParDiff initialAppState (constly' . view) getBody
